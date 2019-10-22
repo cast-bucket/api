0cdd6dca45ea583f70c837c3cf794a2cc5c9e7c6
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-23 00:18:07 +0530

    refactor: use CategoryModel

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,7 +1,7 @@
+import { CategoryModel } from "@models/";
 import { FastifyReply, FastifyRequest } from "fastify";
 import * as http from "http";
 import httpError from "http-errors";
-import { getAllCategories, getCategory } from "../models/category.model";
 
 export const fetchAllCategories = async (
   req: FastifyRequest<http.IncomingMessage>,
@@ -9,7 +9,7 @@ export const fetchAllCategories = async (
 ) => {
   try {
     const showMetadata: boolean | undefined = req.query.meta === "true";
-    const results: any = await getAllCategories(showMetadata);
+    const results: any = await CategoryModel.getAllCategories(showMetadata);
     res.code(200).send(results);
   } catch (error) {
     res.send({ message: error.message, statusCode: error.status });
@@ -24,7 +24,7 @@ export const fetchCategory = async (
     const showMetadata: boolean | undefined = req.query.meta === "true";
     const categoryId: string = req.params.categoryId;
     if (!categoryId) throw httpError(400, "Missing or invalid params: categoryId");
-    const results: any = await getCategory(categoryId, showMetadata);
+    const results: any = await CategoryModel.getCategory(categoryId, showMetadata);
     res.code(200).send(results);
   } catch (error) {
     res.send({ statusCode: error.status || 500, message: error.message });
