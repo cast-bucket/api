7db72ee69e46b2c5021237000cfc1c7ec223836b
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-23 00:11:04 +0530

    refactor: export db models from single file

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+import * as CategoryModel from "./category.model";
+import * as PodcastModel from "./podcast.model";
+import * as UserModel from "./user.model";
+
+export { UserModel, PodcastModel, CategoryModel };
