411f72d3199753af70f0a4266b65e7fe38c5d2d4
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-11-03 12:56:48 +0530

    chore: fix types in config reader

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const readCredentials = (credentialsFileName, fallbackFileName) => {
+const readCredentials = (credentialsFileName: string, fallbackFileName: string) => {
   if (!fs.existsSync(path.join(__dirname, credentialsFileName))) {
     return fs.readFileSync(path.join(__dirname, fallbackFileName)).toString();
   }
